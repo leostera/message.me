272fb1635fcaa894720ef21dc0711b6faab6a6ba
Leandro Ostera <me@leostera.com>
2013-10-14 23:51:11 -0300

    Separated navbar module. Working like a charm :)

---
+++ modules/navbar/directives/navbar.js
@@ -0,0 +1,49 @@
+/**
+ * @ngdoc directive
+ * @name ng2Core.directives:navbar
+ * @description
+ * Show nav bar, show Login with Facebook button.
+ */
+angular.module('ng2Core')
+  .directive('navbar',['OAuth2', '$location', function (OAuth2, $location) {
+    'use strict';
+
+    return {
+      priority: 0,
+      template: require('../views/navbar'),
+      restrict: 'E',
+      replace: true,
+      link: function(scope, element, attr) {
+        scope.menu = []
+        scope.select=false;
+
+        var handleUserData = function (event, data) {
+          scope.menu = []
+          scope.user = typeof data === 'string' ? null : data;
+          if(scope.user && scope.user.username) {
+            scope.menu = ['inbox','compose','about']
+            scope.user.picture = 'https://graph.facebook.com/'+data.username+'/picture';
+          } else {
+            $location.path('/');
+          }
+        };
+
+        scope.$on('ng2auth:login::success', handleUserData);
+        scope.$on('ng2auth:logout::success', handleUserData);
+
+        scope.login = function () {
+          OAuth2.login('facebook');
+        }
+
+        scope.logout = function () {
+          OAuth2.logout();
+        }
+
+        scope.isSelected = function (item) {
+          console.log(scope.selected, item);
+          return scope.selected === item;
+        }
+      }
+    };
+  }
+]);
---
+++ modules/navbar/index.js
@@ -0,0 +1,5 @@
+// auto-exports //
+
+var app = angular.module('navbar', ['ngRoute']);
+
+require('./directives/navbar');
