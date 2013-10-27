6ec360a5f14ced88bd8b9d1c1be8c5cbc9744f08
Leandro Ostera <me@leostera.com>
2013-10-27 08:23:57 -0300

    Update messages list upon receiving a brand new conversation.

--- modules/messages/directives/conversations-list.js
+++ modules/messages/directives/conversations-list.js
@@ -24,52 +24,70 @@ angular.module('mme.messages')
           var messages = data.map(function (d) {
             return JSON.parse(d.Body);
           });
-          console.log("messages list", messages);
           messages.forEach(function (message) {
-            scope.conversations.forEach(function (conversation) {
-              if(message.cid !== conversation._id) return;
-              if(conversation.to._id === message.to) {
-                message.to = conversation.to;
-              } else { //if(conversation.to._id === message.from) {
-                message.from = conversation.to;
-              }
-              if(conversation.from._id === message.to) {
-                message.to = conversation.from;
-              } else { //(conversation.fromessage._id === message.from) {
-                message.from = conversation.from;
-              }
-              console.log("About to unshift", message);
-              conversation.messages.unshift(message);
-            });
-          });
-        });
+            var isNew = !!!(scope.conversations.filter(function (conversation) {
+              return message.cid === conversation._id;
+            }).length)
 
-        ConversationService.list().then(function (conversations) {
-          scope.conversations = scope.conversations.concat(conversations).reverse();
-          scope.conversations.forEach(function (c) {
-            scope.users.forEach(function (u) {
-              if(c.from._id === u._id) {
-                c.from = u;
-              }
-              if(c.to._id === u._id) {
-                c.to = u;
-              }
-            });
-            c.messages = c.messages.reverse();
-            c.messages.forEach(function (m) {
-              if(c.to._id === m.to) {
-                m.to = c.to;
-              } else { //if(c.to._id === m.from) {
-                m.from = c.to;
-              }
-              if(c.from._id === m.to) {
-                m.to = c.from;
-              } else { //(c.from._id === m.from) {
-                m.from = c.from;
-              }
-            });
+            console.log(isNew);
+
+            if(isNew) {
+              updateConversations();
+            } else {
+              scope.conversations.forEach(function (conversation) {
+                if(message.cid !== conversation._id) return;
+                if(conversation.to._id === message.to) {
+                  message.to = conversation.to;
+                } else { //if(conversation.to._id === message.from) {
+                  message.from = conversation.to;
+                }
+                if(conversation.from._id === message.to) {
+                  message.to = conversation.from;
+                } else { //(conversation.fromessage._id === message.from) {
+                  message.from = conversation.from;
+                }
+                console.log("About to unshift", message);
+                conversation.messages.unshift(message);
+              });
+            }
           });
         });
+        
+        var updateConversations = function () {
+          ConversationService.list().then(function (conversations) {
+            scope.conversations = scope.conversations.concat(conversations).reverse();
+            var id = scope.selectedConversation._id;
+            scope.conversations = _.unique(scope.conversations, '_id');
+            scope.conversations.forEach(function (c) {
+              if(c._id == id) {
+                scope.selectedConversation = c;
+                scope.selectedConversation.selected = true;
+              }
+              scope.users.forEach(function (u) {
+                if(c.from._id === u._id) {
+                  c.from = u;
+                }
+                if(c.to._id === u._id) {
+                  c.to = u;
+                }
+              });
+              c.messages = c.messages.reverse();
+              c.messages.forEach(function (m) {
+                if(c.to._id === m.to) {
+                  m.to = c.to;
+                } else { //if(c.to._id === m.from) {
+                  m.from = c.to;
+                }
+                if(c.from._id === m.to) {
+                  m.to = c.from;
+                } else { //(c.from._id === m.from) {
+                  m.from = c.from;
+                }
+              });
+            });
+          });
+        }
+        updateConversations();
 
         scope.select = function (conversation) {
           var index = scope.conversations.indexOf(conversation);
